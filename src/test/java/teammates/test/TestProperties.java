8f46a25248a49415258e3c250ce6e8544bb3f390
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2023-01-02 21:39:22 +0800

    Remove useless code

--- src/test/java/teammates/test/TestProperties.java
+++ src/test/java/teammates/test/TestProperties.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-import teammates.common.util.Config;
 import teammates.common.util.StringHelper;
 
 /**
@@ -22,18 +21,6 @@ public final class TestProperties {
 
     /** The value of "test.localdatastore.port" in test.properties file. */
     public static final int TEST_LOCALDATASTORE_PORT;
-
-    /** The value of "test.localpostgres.port" in test.properties file. */
-    public static final int TEST_LOCALPOSTGRES_PORT;
-
-    /** The value of "test.localpostgres.username" in test.properties file. */
-    public static final String TEST_LOCALPOSTGRES_USERNAME;
-
-    /** The value of "test.localpostgres.password" in test.properties file. */
-    public static final String TEST_LOCALPOSTGRES_PASSWORD;
-
-    /** The value of "test.localpostgres.DB" in test.properties file. */
-    public static final String TEST_LOCALPOSTGRES_DB;
 
     /** Indicates whether auto-update snapshot mode is activated. */
     public static final boolean IS_SNAPSHOT_UPDATE;
@@ -54,10 +41,6 @@ public final class TestProperties {
 
             IS_SNAPSHOT_UPDATE = Boolean.parseBoolean(prop.getProperty("test.snapshot.update", "false"));
             TEST_LOCALDATASTORE_PORT = Integer.parseInt(prop.getProperty("test.localdatastore.port"));
-            TEST_LOCALPOSTGRES_PORT = Integer.parseInt(prop.getProperty("test.localpostgres.port", "5433"));
-            TEST_LOCALPOSTGRES_USERNAME = prop.getProperty("test.localpostgres.username", "teammates-test");
-            TEST_LOCALPOSTGRES_PASSWORD = prop.getProperty("test.localpostgres.password", "teammates-test");
-            TEST_LOCALPOSTGRES_DB = prop.getProperty("test.localpostgres.db", "teammates-test");
             SEARCH_SERVICE_HOST = prop.getProperty("test.search.service.host");
 
         } catch (IOException | NumberFormatException e) {
@@ -65,10 +48,6 @@ public final class TestProperties {
         }
     }
 
-    public static String getTestPostgresConnectionUrl() {
-        return "jdbc:postgresql://localhost:"
-                + TestProperties.TEST_LOCALPOSTGRES_PORT + "/" + TestProperties.TEST_LOCALPOSTGRES_DB;
-    }
 
     public static boolean isSearchServiceActive() {
         return !StringHelper.isEmpty(SEARCH_SERVICE_HOST);
