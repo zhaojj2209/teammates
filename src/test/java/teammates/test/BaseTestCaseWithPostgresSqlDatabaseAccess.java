f80bc186aa15afefc4063a1696b787f19f21c1ce
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2023-01-02 21:45:35 +0800

    Remove commented code

--- src/test/java/teammates/test/BaseTestCaseWithPostgresSqlDatabaseAccess.java
+++ src/test/java/teammates/test/BaseTestCaseWithPostgresSqlDatabaseAccess.java
@@ -2,7 +2,6 @@ package teammates.test;
 
 import teammates.common.datatransfer.sqlattributes.CourseAttributes;
 import teammates.common.datatransfer.sqlattributes.EntityAttributes;
-import teammates.common.datatransfer.sqlattributes.InstructorAttributes;
 import teammates.common.util.JsonUtils;
 
 public abstract class BaseTestCaseWithPostgresSqlDatabaseAccess extends BaseTestCase{
@@ -43,68 +42,9 @@ public abstract class BaseTestCaseWithPostgresSqlDatabaseAccess extends BaseTest
         }
     }
 
-//    private void equalizeIrrelevantData(AccountAttributes expected, AccountAttributes actual) {
-//        // Ignore time field as it is stamped at the time of creation in testing
-//        expected.setCreatedAt(actual.getCreatedAt());
-//    }
-//
-//    private void equalizeIrrelevantData(StudentProfileAttributes expected, StudentProfileAttributes actual) {
-//        expected.setModifiedDate(actual.getModifiedDate());
-//    }
-
     private void equalizeIrrelevantData(CourseAttributes expected, CourseAttributes actual) {
         // Ignore time field as it is stamped at the time of creation in testing
         expected.setCreatedAt(actual.getCreatedAt());
     }
 
-//    private void equalizeIrrelevantData(FeedbackQuestionAttributes expected, FeedbackQuestionAttributes actual) {
-//        expected.setId(actual.getId());
-//    }
-//
-//    private void equalizeIrrelevantData(FeedbackResponseAttributes expected, FeedbackResponseAttributes actual) {
-//        expected.setId(actual.getId());
-//    }
-//
-//    private void equalizeIrrelevantData(FeedbackSessionAttributes expected, FeedbackSessionAttributes actual) {
-//        expected.setCreatedTime(actual.getCreatedTime());
-//        // Not available in FeedbackSessionData and thus ignored
-//        expected.setCreatorEmail(actual.getCreatorEmail());
-//    }
-
-    private void equalizeIrrelevantData(InstructorAttributes expected, InstructorAttributes actual) {
-        // pretend keys match because the key is generated only before storing into database
-        if (actual.getKey() != null) {
-            expected.setKey(actual.getKey());
-        }
-    }
-
-//    private void equalizeIrrelevantData(StudentAttributes expected, StudentAttributes actual) {
-//        // For these fields, we consider null and "" equivalent.
-//        if (expected.getGoogleId() == null && actual.getGoogleId().isEmpty()) {
-//            expected.setGoogleId("");
-//        }
-//        if (expected.getTeam() == null && actual.getTeam().isEmpty()) {
-//            expected.setTeam("");
-//        }
-//        if (expected.getComments() == null && actual.getComments().isEmpty()) {
-//            expected.setComments("");
-//        }
-//
-//        // pretend keys match because the key is generated only before storing into database
-//        if (actual.getKey() != null) {
-//            expected.setKey(actual.getKey());
-//        }
-//    }
-
-//    private void equalizeIrrelevantData(DeadlineExtensionAttributes expected, DeadlineExtensionAttributes actual) {
-//        // Ignore time field as it is stamped at the time of creation in testing
-//        expected.setCreatedAt(actual.getCreatedAt());
-//        expected.setUpdatedAt(actual.getUpdatedAt());
-//    }
-//
-//    private void equalizeIrrelevantData(NotificationAttributes expected, NotificationAttributes actual) {
-//        // Ignore time field as it is stamped at the time of creation in testing
-//        expected.setCreatedAt(actual.getCreatedAt());
-//        expected.setUpdatedAt(actual.getUpdatedAt());
-//    }
 }
