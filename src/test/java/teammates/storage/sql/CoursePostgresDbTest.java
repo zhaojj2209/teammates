5219709fffbf80ed4c6e9142229a6585982f2bc4
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2023-01-02 21:33:25 +0800

    Integrate TestContainer

---
+++ src/test/java/teammates/storage/sql/CoursePostgresDbTest.java
@@ -0,0 +1,47 @@
+package teammates.storage.sql;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import teammates.test.BaseTestCaseWithLocalPostgresSqlDatabaseAccess;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * SUT: {@link CoursesDb}.
+ */
+public class CoursePostgresDbTest extends BaseTestCaseWithLocalPostgresSqlDatabaseAccess {
+    private final CoursesDb coursesDb = CoursesDb.inst();
+    private static PostgreSQLContainer pgsql;
+
+    @BeforeClass
+    public static void init() {
+        pgsql = new PostgreSQLContainer<>("postgres:14.4")
+                .withDatabaseName("pgsql-test")
+                .withUsername("pgsql-test")
+                .withPassword("pgsql-test");
+        pgsql.start();
+        System.out.println(pgsql.getExposedPorts());
+        System.out.println(pgsql.getLivenessCheckPortNumbers());
+    }
+
+    @AfterClass
+    public static void destroy() {
+        pgsql.close();
+    }
+
+    @Test
+    public void testContainer() throws Exception {
+        DataSource ds = getDataSource(pgsql);
+        Statement stmt = ds.getConnection().createStatement();
+        stmt.execute("CREATE TABLE test (number INTEGER)");
+        stmt.execute("INSERT INTO test VALUES (1)");
+        stmt.execute("SELECT * FROM test;");
+        ResultSet resultSet = stmt.getResultSet();
+        resultSet.next();
+        assertEquals(1, resultSet.getInt(1));
+    }
+}
--- src/test/java/teammates/test/BaseTestCaseWithLocalPostgresSqlDatabaseAccess.java
+++ src/test/java/teammates/test/BaseTestCaseWithLocalPostgresSqlDatabaseAccess.java
@@ -1,32 +1,25 @@
 package teammates.test;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.testng.annotations.BeforeSuite;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 import teammates.common.datatransfer.sqlattributes.CourseAttributes;
 import teammates.logic.api.LogicNewExtension;
-import teammates.storage.sqlentity.Course;
-import teammates.storage.sqlentity.Instructor;
+
+import javax.sql.DataSource;
 
 public abstract class BaseTestCaseWithLocalPostgresSqlDatabaseAccess extends BaseTestCaseWithPostgresSqlDatabaseAccess {
     private static SessionFactory sessionFactory;
     private final LogicNewExtension logic = new LogicNewExtension();
 
-    @BeforeSuite
-    public void setupSessionFactory() {
-        Configuration cfg = new Configuration()
-                .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
-                .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
-                .setProperty("hibernate.connection.username", TestProperties.TEST_LOCALPOSTGRES_USERNAME)
-                .setProperty("hibernate.connection.password", TestProperties.TEST_LOCALPOSTGRES_PASSWORD)
-                .setProperty("hibernate.connection.url", TestProperties.getTestPostgresConnectionUrl())
-                .setProperty("hibernate.hbm2ddl.auto", "validate")
-                .setProperty("show_sql", "true")
-                .addPackage("teammates.storage.sqlentity")
-                .addAnnotatedClass(Course.class)
-                .addAnnotatedClass(Instructor.class);
-
-        sessionFactory = cfg.buildSessionFactory();
+    protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(container.getJdbcUrl());
+        hikariConfig.setUsername(container.getUsername());
+        hikariConfig.setPassword(container.getPassword());
+        hikariConfig.setDriverClassName(container.getDriverClassName());
+        return new HikariDataSource(hikariConfig);
     }
 
     @Override
