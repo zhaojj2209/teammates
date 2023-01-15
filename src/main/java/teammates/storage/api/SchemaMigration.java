11eadf346571f2bc78d2b31e3fe89bffacaaa367
LI JIANHAN <59337195+jianhandev@users.noreply.github.com>
2023-01-15 19:34:04 +0800

    Revert "Add FlywayUtil to allow integration of Flyway with TestContainers"

--- src/main/java/teammates/storage/api/SchemaMigration.java
+++ src/main/java/teammates/storage/api/SchemaMigration.java
@@ -3,7 +3,6 @@ package teammates.storage.api;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.internal.info.MigrationInfoDumper;
 import teammates.common.util.Config;
-import teammates.common.util.FlywayUtil;
 import teammates.common.util.Logger;
 
 public class SchemaMigration {
@@ -11,9 +10,17 @@ public class SchemaMigration {
     private static final Logger log = Logger.getLogger();
 
     public static void main(String[] args) {
-        Flyway flyway = FlywayUtil.getFlywayInst(
-                Config.getDbConnectionUrl(), Config.getDbUsername(), Config.getDbPassword());
+        Flyway flyway = Flyway.configure().dataSource(
+                Config.getDbConnectionUrl(),
+                Config.APP_LOCALPOSTGRES_USERNAME,
+                Config.APP_LOCALPOSTGRES_PASSWORD)
+                .baselineVersion(Config.APP_FLYWAY_BASELINEVERSION)
+                .baselineOnMigrate(true)
+                .locations("classpath:db/migration")
+                .load();
+
         flyway.migrate();
+
         log.info(MigrationInfoDumper.dumpToAsciiTable(flyway.info().all()));
     }
 }
